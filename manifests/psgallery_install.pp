9bf1f1c9e5956681f13c16392e314dec0841b068
eshwarprasadhu <51907579+eshwarprasadhu@users.noreply.github.com>
2019-06-26 14:29:04 +0530

    Create psgallery_install.pp
    Nuget package provider, ensure the PowerShell Gallery repository is configured and trusted, and install two modules (one using the WindowsPowerShell provider and one using the PowerShellCore provider)

---
+++ manifests/psgallery_install.pp
@@ -0,0 +1,26 @@
+class psgallery_install {
+
+pspackageprovider {'Nuget':
+  ensure => 'present'
+}
+
+psrepository { 'PSGallery':
+  ensure              => present,
+  source_location     => 'https://www.powershellgallery.com/api/v2/',
+  installation_policy => 'trusted',
+}
+
+package { 'xPSDesiredStateConfiguration':
+  ensure   => latest,
+  provider => 'windowspowershell',
+  source   => 'PSGallery',
+}
+
+package { 'Pester':
+  ensure   => latest,
+  provider => 'powershellcore',
+  source   => 'PSGallery',
+}
+
+}
+
