b0bd6b18af5cc1320d9f50160a82b6b4c94dded7
eshwarprasadhu <51907579+eshwarprasadhu@users.noreply.github.com>
2019-06-27 17:00:41 +0530

    Create myserviceaccounts.pp
    Added to override the dsc_domainadministratorcredential

---
+++ manifests/myserviceaccounts.pp
@@ -0,0 +1,24 @@
+class myserviceaccounts inherits  {
+
+	#SQL service account creation (Active Directory)
+	dsc_xaduser{'SvcSQLAccount':
+		dsc_domainname => $domain,
+		dsc_domainadministratorcredential => undef,
+		dsc_username => $sqlservicecredential_username,
+		dsc_password => {'user' => $sqlservicecredential_username, 'password' => $sqlservicecredential_password},
+		dsc_ensure => 'Present',
+		require => Dsc_windowsfeature['RSAT-AD-Powershell']
+	}
+
+
+	#SQL Agent service account creation (Active Directory)
+	dsc_xaduser{'SvcSQLAgentAccount':
+		dsc_domainname => $domain,
+		dsc_domainadministratorcredential => undef,
+		dsc_username => $sqlagentservicecredential_username,
+		dsc_password => {'user' => $sqlagentservicecredential_username, 'password' => $sqlagentservicecredential_password},
+		dsc_ensure => 'Present',
+		require => Dsc_windowsfeature['RSAT-AD-Powershell']
+	}
+
+}
