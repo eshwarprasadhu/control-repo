e8a44e17676777bb884a66bf1461b07cba3d2962
eshwarprasadhu <51907579+eshwarprasadhu@users.noreply.github.com>
2019-06-28 12:16:00 +0530

    Fix for fail_on_duplicate_type_and_title
    2019-06-28T12:00:41.239+05:30 ERROR [qtp93076268-117] [puppetserver] Puppet Evaluation Error: Error while evaluating a Resource Statement, Evaluation Error: Error while evaluating a Resource Statement, Duplicate declaration: Dsc_xaduser[SvcSQLAccount] is already declared at (file: /etc/puppetlabs/code/environments/production/modules/sqlserveralwayson/manifests/serviceaccounts.pp, line: 10); cannot redeclare (file: /etc/puppetlabs/code/environments/production/manifests/myserviceaccounts.pp, line: 4) (file: /etc/puppetlabs/code/environments/production/manifests/myserviceaccounts.pp, line: 4, column: 2) on node ns-coc2-vm.nousinfo.com
    2019-06-28T12:00:41.240+05:30 ERROR [qtp93076268-117] [puppetserver] Puppet Server Error: Evaluation Error: Error while evaluating a Resource Statement, Evaluation Error: Error while evaluating a Resource Statement, Duplicate declaration: Dsc_xaduser[SvcSQLAccount] is already declared at (file: /etc/puppetlabs/code/environments/production/modules/sqlserveralwayson/manifests/serviceaccounts.pp, line: 10); cannot redeclare (file: /etc/puppetlabs/code/environments/production/manifests/myserviceaccounts.pp, line: 4) (file: /etc/puppetlabs/code/environments/production/manifests/myserviceaccounts.pp, line: 4, column: 2) on node ns-coc2-vm.nousinfo.com
    /opt/puppetlabs/puppet/lib/ruby/vendor_ruby/puppet/resource/catalog.rb:604:in `fail_on_duplicate_type_and_title'

--- manifests/myserviceaccounts.pp
+++ manifests/myserviceaccounts.pp
@@ -1,9 +1,9 @@
 class myserviceaccounts inherits sqlserveralwayson::serviceaccounts {
 
 	#SQL service account creation (Active Directory)
-	dsc_xaduser{'SvcSQLAccount':
+	dsc_xaduser{'SvcSQLAccount19':
 		dsc_domainname => $domain,
-		dsc_domainadministratorcredential => '',
+		dsc_domainadministratorcredential => undef,
 		dsc_username => $sqlservicecredential_username,
 		dsc_password => {'user' => $sqlservicecredential_username, 'password' => $sqlservicecredential_password},
 		dsc_ensure => 'Present',
@@ -12,9 +12,9 @@ class myserviceaccounts inherits sqlserveralwayson::serviceaccounts {
 
 
 	#SQL Agent service account creation (Active Directory)
-	dsc_xaduser{'SvcSQLAgentAccount':
+	dsc_xaduser{'SvcSQLAgentAccount19':
 		dsc_domainname => $domain,
-		dsc_domainadministratorcredential => '',
+		dsc_domainadministratorcredential => undef,
 		dsc_username => $sqlagentservicecredential_username,
 		dsc_password => {'user' => $sqlagentservicecredential_username, 'password' => $sqlagentservicecredential_password},
 		dsc_ensure => 'Present',
