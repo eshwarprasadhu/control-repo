6cb1b225c1464abb74f777174bb028df5cf76229
eshwarprasadhu <51907579+eshwarprasadhu@users.noreply.github.com>
2019-07-23 11:23:58 +0530

    Enabled dac and cloud witness
    Enabled domainadministratorcredential (dac) and cloud witness

--- manifests/clusterconfig.pp
+++ manifests/clusterconfig.pp
@@ -6,15 +6,14 @@ class sqlserveralwayson::clusterconfig inherits sqlserveralwayson {
     dsc_xcluster{'CreateFailoverCluster':
       dsc_name => $clusterName,
       dsc_staticipaddress => $clusterIP,
-      #dsc_domainadministratorcredential => {'user' => $setup_svc_username, 'password' => $setup_svc_password}
+      dsc_domainadministratorcredential => {'user' => $setup_svc_username, 'password' => $setup_svc_password}
     }
 
-    #File share whitness configuration
-    #Warning, bug https://github.com/PowerShell/xFailOverCluster/issues/35 on Windows 2016
-    dsc_xclusterquorum{'SetQuorumToNodeAndDiskMajority':
+    dsc_xclusterquorum{'SetQuorumToNodeAndCloudMajority':
       dsc_issingleinstance => 'Yes',
-      dsc_type => 'NodeAndFileShareMajority',
+      dsc_type => 'NodeAndCloudMajority',
       dsc_resource => $fileShareWitness,
+      dsc_storageaccountaccesskey => 'tVuGtuNmbwGeqYcS2r+9JyLjavECkGRPgEUPpN4sPrkQ5swbcrkjxBYzqIE1eh9EXCB63/HVxU3LuOxBxvIhPQ==',
       require => Dsc_xcluster['CreateFailoverCluster']
     }
 
@@ -29,7 +28,7 @@ class sqlserveralwayson::clusterconfig inherits sqlserveralwayson {
     dsc_xcluster{'JoinCluster':
       dsc_name => $clusterName,
       dsc_staticipaddress => $clusterIP,
-      #dsc_domainadministratorcredential => {'user' => $setup_svc_username, 'password' => $setup_svc_password},
+      dsc_domainadministratorcredential => {'user' => $setup_svc_username, 'password' => $setup_svc_password},
       require => Dsc_xwaitforcluster['SecondaryReplicaWaitForCluster']
     }
   }
